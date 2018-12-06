3b85f81f01f3c8296ff8a0c0298a43410e55eee0
sudip dig <sudip dig>
2018-12-06 13:24:48 +0530

    Initial commit

---
+++ app/src/main/java/com/logicbeanzs/uberstylepolylineanimation/MapAnimator.java
@@ -0,0 +1,249 @@
+package com.logicbeanzs.uberstylepolylineanimation;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.IntegerRes;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.List;
+
+/**
+ * Created by amal.chandran on 22/12/16.
+ */
+
+public class MapAnimator {
+
+    private static MapAnimator mapAnimator;
+    private static Polyline backgroundPolyline;
+
+    private static Polyline foregroundPolyline;
+
+    private static PolylineOptions optionsForeground;
+
+    private static AnimatorSet firstRunAnimSet;
+
+    private static AnimatorSet secondLoopRunAnimSet;
+
+    private static int GREY = Color.parseColor("#FFA7A6A6");
+    private static int BLACK = Color.BLACK;
+    private static int PERCENT_COMPLETION = 2500;
+    private static int COLOR_FILL_ANIMATION = 1800;
+    private static int FOREGROUND_TIME = 2000;
+    private static int DELAY_TIME = 200;
+    private MapAnimator(){
+
+    }
+
+
+    public static MapAnimator getInstance(Context context){
+        if(mapAnimator == null) mapAnimator = new MapAnimator();
+        return mapAnimator;
+    }
+
+    public static void setPercentCompletion(@IntegerRes int time) {
+        PERCENT_COMPLETION = time;
+    }
+
+    public static void setColorFillCompletion(@IntegerRes int time) {
+        COLOR_FILL_ANIMATION = time;
+    }
+    public static void setPrimaryLineCompletion(@IntegerRes int time) {
+        FOREGROUND_TIME = time;
+    }
+    public static void setDelayTime(@IntegerRes int time) {
+        DELAY_TIME = time;
+    }
+
+    public static void setPrimaryLineColor(@ColorRes int color) {
+        BLACK = color;
+    }
+    public static void setSecondaryLineColor(@ColorRes int color) {
+        GREY = color;
+    }
+    public static void animateRoute(Context context, GoogleMap googleMap, List<LatLng> bangaloreRoute) {
+        if (firstRunAnimSet == null){
+            firstRunAnimSet = new AnimatorSet();
+        } else {
+            firstRunAnimSet.removeAllListeners();
+            firstRunAnimSet.end();
+            firstRunAnimSet.cancel();
+
+            firstRunAnimSet = new AnimatorSet();
+        }
+        if (secondLoopRunAnimSet == null) {
+            secondLoopRunAnimSet = new AnimatorSet();
+        } else {
+            secondLoopRunAnimSet.removeAllListeners();
+            secondLoopRunAnimSet.end();
+            secondLoopRunAnimSet.cancel();
+
+            secondLoopRunAnimSet = new AnimatorSet();
+        }
+        //Reset the polylines
+        if (foregroundPolyline != null) foregroundPolyline.remove();
+        if (backgroundPolyline != null) backgroundPolyline.remove();
+
+
+        PolylineOptions optionsBackground = new PolylineOptions().add(bangaloreRoute.get(0)).color(GREY).width(8);
+        backgroundPolyline = googleMap.addPolyline(optionsBackground);
+
+        optionsForeground = new PolylineOptions().add(bangaloreRoute.get(0)).color(BLACK).width(8);
+        foregroundPolyline = googleMap.addPolyline(optionsForeground);
+
+        final ValueAnimator percentageCompletion = ValueAnimator.ofInt(0, 100);
+        percentageCompletion.setDuration(PERCENT_COMPLETION);
+        percentageCompletion.setInterpolator(new DecelerateInterpolator());
+        percentageCompletion.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                List<LatLng> foregroundPoints = backgroundPolyline.getPoints();
+
+                int percentageValue = (int) animation.getAnimatedValue();
+                int pointcount = foregroundPoints.size();
+                int countTobeRemoved = (int) (pointcount * (percentageValue / 100.0f));
+                List<LatLng> subListTobeRemoved = foregroundPoints.subList(0, countTobeRemoved);
+                subListTobeRemoved.clear();
+
+                foregroundPolyline.setPoints(foregroundPoints);
+            }
+        });
+        percentageCompletion.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                foregroundPolyline.setColor(GREY);
+                foregroundPolyline.setPoints(backgroundPolyline.getPoints());
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), GREY, BLACK);
+        colorAnimation.setInterpolator(new AccelerateInterpolator());
+        colorAnimation.setDuration(COLOR_FILL_ANIMATION); // milliseconds
+
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                foregroundPolyline.setColor((int) animator.getAnimatedValue());
+            }
+
+        });
+
+        ObjectAnimator foregroundRouteAnimator = ObjectAnimator.ofObject(context, "routeIncreaseForward", new RouteEvaluator(), bangaloreRoute.toArray());
+        foregroundRouteAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        foregroundRouteAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                backgroundPolyline.setPoints(foregroundPolyline.getPoints());
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        foregroundRouteAnimator.setDuration(FOREGROUND_TIME);
+//        foregroundRouteAnimator.start();
+
+        firstRunAnimSet.playSequentially(foregroundRouteAnimator,
+                percentageCompletion);
+        firstRunAnimSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                secondLoopRunAnimSet.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        secondLoopRunAnimSet.playSequentially(colorAnimation,
+                percentageCompletion);
+        secondLoopRunAnimSet.setStartDelay(DELAY_TIME);
+
+        secondLoopRunAnimSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                secondLoopRunAnimSet.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        firstRunAnimSet.start();
+    }
+
+    /**
+     * This will be invoked by the ObjectAnimator multiple times. Mostly every 16ms.
+     **/
+    public void setRouteIncreaseForward(LatLng endLatLng) {
+        List<LatLng> foregroundPoints = foregroundPolyline.getPoints();
+        foregroundPoints.add(endLatLng);
+        foregroundPolyline.setPoints(foregroundPoints);
+    }
+
+}
+
